2acf95195090a35f33b1d202148896d786864c6b
dimituri <none@none>
2010-05-19 15:28:42 +0300

    Added basic scope logic

--- lib/scope.rb
+++ lib/scope.rb
@@ -1,2 +1,27 @@
 class Scope
+  @@top = nil
+  
+  def initialize
+    @parent = @@top
+    @@top = self
+    @entries = {}
+  end
+  
+  def get(id)
+    if @entries.include?(id) then @entries[id]
+    elsif @parent then @parent.get(id)
+    else nil
+    end
+  end
+  
+  def set(id)
+    @entries[id] = {
+      :offset => @entries.size,
+      :global => @parent == nil
+    } unless @entries.include?(id)
+  end
+  
+  def close
+    @@top = @parent
+  end
 end
